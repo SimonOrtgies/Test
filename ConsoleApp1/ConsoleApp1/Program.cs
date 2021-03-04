11e5f08d5f89bf7c6591a1947e0cb08f6e54d915
SimonOrtgies <simon.ortgies@student.jade-hs.de>
2021-03-04 13:08:14 +0100

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+/*
+ Hello World Programm
+ Version 1.0
+ letzte Aenderung 17.09.2019
+ Kai Mecke
+*/
+
+namespace Hello_World_Mecke
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Hier beginnt das Hauptprogramm 
         {
-            Console.WriteLine("LOL");
+            // Ausgabe Benutzer 1
+            Console.WriteLine("Hello World von XXX");
 
-        }
+            // Ausgabe Benutzer 2
+            Console.WriteLine("Nǐ hǎo, shìjiè");
+
+            // Ausgabe Benutzer 3
+            Console.WriteLine("Hallo Welt von XXX");
+
+            // Ausgabe Benutzer 4
+            Console.WriteLine("marhabaan bialealam");
+
+            // warte bis der User eine Taste drueckt
+            Console.ReadKey();
+
+        } // Hier endet das Hauptprogramm
     }
 }
