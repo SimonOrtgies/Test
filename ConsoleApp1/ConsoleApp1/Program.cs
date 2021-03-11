d92cf28fe97a82a2e09aea5d637ddf0ecd2cc78b
SimonOrtgies <simon.ortgies@student.jade-hs.de>
2021-03-11 10:53:13 +0100

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,14 +14,13 @@ namespace Hello_World_Mecke
         static void Main(string[] args) // Hier beginnt das Hauptprogramm 
         {
             // Ausgabe Benutzer 1
-            Console.WriteLine("Hello World von XXX");
+            Console.WriteLine("Hello World von YYY");
 
             // Ausgabe Benutzer 2
             Console.WriteLine("Nǐ hǎo, shìjiè");
 
             // Ausgabe Benutzer 3
             Console.WriteLine("Moin Welt von XXX");
-
 
             // Ausgabe Benutzer 4
             Console.WriteLine("marhabaan bialealam");
